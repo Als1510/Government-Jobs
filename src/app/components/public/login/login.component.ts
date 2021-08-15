447334a4ba28b070a09e7a3d5367e4986c5928fd
Als1510 <lekhrajsharmaas@gmail.com>
2021-08-15 19:25:11 +0530

    Build the project

--- src/app/components/public/login/login.component.ts
+++ src/app/components/public/login/login.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from '../../../services/auth.service'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  login = new Login('anshul@gmail.com', 'anshul123');
+  login = new Login('', '');
   isLoggedIn = false;
   isLoginFailed = false;
   msgClass = 'danger';
